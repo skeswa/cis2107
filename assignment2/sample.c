5600ea18d2477e2d7699be869b0610c20f1d4b1a
Sandile <tue95828@temple.edu>
2013-09-13 10:49:02 -0400

    Added functional assignment 2 source

---
+++ assignment2/sample.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+
+int main(void) {
+	/* What is going on here */
+	int iDontKnowSire, whyDoYouWantToKnow;
+	/* Rufus, I want to know because it is my castle */
+	if (iDontKnowSire) {
+		printf("Then I cannot help you\n");
+	}
+	/* Well thats a shame isn't it, I suppose I will have to keep talking
+		forever and ever and ever and ever and ever MWAHAHAHAHA
+}
